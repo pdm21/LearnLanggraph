2485c97ff0f94765d8b8fa55bc5b67a0911a7f2e
pdm21 <pandsmar@gmail.com>
2025-01-10 14:06:49 -0500

    google drive api successfully fetching files from provided folder_id

--- GoogleDriveAgent/agent.py
+++ GoogleDriveAgent/agent.py
@@ -1,16 +1,27 @@
 from langchain_google_community import GoogleDriveLoader
+from langchain_googledrive.document_loaders import GoogleDriveLoader
 
 loader = GoogleDriveLoader(
     folder_id="13LE8SEQNHfmfHxTaNEA9hzqzPeODcsKB",
-    credentials_path="./.credentials/credentials.json",
-    token_path="./.credentials/token.json",
-    file_types=["document", "sheet"],
-    recursive=True,
+    credentials_path="credentials.json",
+    token_path="token.json",
+    load_auth=True,
+    # Optional: configure whether to load authorized identities for each Document.
 )
 
 docs = loader.load()
-print("len docs: ", len(docs))
+print(len(docs))
 
+# loader = GoogleDriveLoader(
+#     folder_id="13LE8SEQNHfmfHxTaNEA9hzqzPeODcsKB",
+#     credentials_path="./.credentials/credentials.json",
+#     token_path="./.credentials/token.json",
+#     file_types=["document", "sheet"],
+#     recursive=True,
+# )
+
+# docs = loader.load()
+# print("len docs: ", len(docs))
 
 # docs:
 # https://python.langchain.com/docs/integrations/document_loaders/google_drive/
