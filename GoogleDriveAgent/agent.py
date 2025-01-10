0baafcf116859a6a648f5faa6cfc7e9c6b535458
pdm21 <pandsmar@gmail.com>
2025-01-10 14:07:06 -0500

    cleanup

--- GoogleDriveAgent/agent.py
+++ GoogleDriveAgent/agent.py
@@ -12,16 +12,5 @@ loader = GoogleDriveLoader(
 docs = loader.load()
 print(len(docs))
 
-# loader = GoogleDriveLoader(
-#     folder_id="13LE8SEQNHfmfHxTaNEA9hzqzPeODcsKB",
-#     credentials_path="./.credentials/credentials.json",
-#     token_path="./.credentials/token.json",
-#     file_types=["document", "sheet"],
-#     recursive=True,
-# )
-
-# docs = loader.load()
-# print("len docs: ", len(docs))
-
 # docs:
 # https://python.langchain.com/docs/integrations/document_loaders/google_drive/
